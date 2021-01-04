e78d5145b52182e520c2689086fac9a70e39b124
Yehonathan Sharvit <viebel@gmail.com>
2021-01-04 06:57:18 +0200

    Change folding method

--- init.el
+++ init.el
@@ -565,7 +565,7 @@ It should only modify the values of Spacemacs settings."
 
    ;; Code folding method. Possible values are `evil', `origami' and `vimish'.
    ;; (default 'evil)
-   dotspacemacs-folding-method 'vimish
+   dotspacemacs-folding-method 'evil
 
    ;; If non-nil `smartparens-strict-mode' will be enabled in programming modes.
    ;; (default nil)
