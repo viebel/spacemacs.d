4303ba082c5e4383bb3e8b84dadd4c4968e4cd82
Yehonathan Sharvit <viebel@gmail.com>
2021-01-05 10:12:31 +0200

    Copy and paste clipboard via special key bindings

--- init.el
+++ init.el
@@ -1514,6 +1514,57 @@ before packages are loaded."
     (add-to-list 'evil-cp-additional-bindings '("M-b" . evil-cp-wrap-previous-square))
     (evil-cp-set-additional-bindings))
 
+  ;; Copy and paste from clipboard
+  ;; https://github.com/syl20bnr/spacemacs/issues/2222#issuecomment-481155006
+  (cond
+   ;; OS X
+   ((string-equal system-type "darwin") ; Mac OS X
+    (progn
+      (setq save-to-clipboard-cmd "pbcopy")
+      (setq paste-from-clipboard-cmd "pbpaste")
+      )
+    )
+   ;; Linux
+   ((string-equal system-type "gnu/linux") ; linux
+    (progn
+      (setq save-to-clipboard-cmd "xsel -i -b")
+      (setq paste-from-clipboard-cmd "xsel -o -b")
+      )
+    )
+   )
+
+  (defun copy-to-clipboard ()
+    "Copies selection to x-clipboard."
+    (interactive)
+    (if (display-graphic-p)
+        (progn
+          (message "Yanked region to x-clipboard!")
+          (call-interactively 'clipboard-kill-ring-save)
+          )
+      (if (region-active-p)
+          (progn
+            (shell-command-on-region (region-beginning) (region-end) save-to-clipboard-cmd)
+            (message "Yanked region to clipboard!")
+            (deactivate-mark))
+        (message "No region active; can't yank to clipboard!")))
+    )
+
+  (defun paste-from-clipboard ()
+    "Pastes from x-clipboard."
+    (interactive)
+    (if (display-graphic-p)
+        (progn
+          (clipboard-yank)
+          (message "graphics active")
+          )
+      (insert (shell-command-to-string paste-from-clipboard-cmd))
+      )
+    )
+  (evil-leader/set-key "o y" 'copy-to-clipboard)
+  (evil-leader/set-key "o p" 'paste-from-clipboard)
+  ;;; ;;;
+
+
   ;; TODO: Spacemacs pull request with these keybindings, updating REPL intro text with details
   ;; You can remove this message with the <M-x cider-repl-clear-help-banner> command.
   ;; You can disable it from appearing on start by setting
