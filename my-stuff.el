9cbef0ff10ba36b723a1901c173aa535ce445133
Yehonathan Sharvit <viebel@gmail.com>
2021-07-05 08:18:08 +0300

    My stuff

---
+++ my-stuff.el
@@ -0,0 +1,39 @@
+(require 'parseclj)
+
+(defun my-stuff-first-form ()
+  (save-excursion
+    (goto-char 1)
+    (setq my-ff (parseclj-parse-clojure :read-one t))))
+
+
+(defun my-stuff-children (form)
+  (let (res)
+    (while form
+      (let ((elem (car form)))
+        (if (eq :children (car elem))
+            (progn
+              (setq res (cdr elem))
+              (setq form nil))))
+      (setq form (cdr form)))
+    res))
+
+(defun my-stuff-node (form node-name)
+  (let (res)
+    (while form
+      (let ((elem (car form)))
+        (if (eq node-name (car elem))
+            (progn
+              (setq res (cdr elem))
+              (setq form nil))))
+      (setq form (cdr form)))
+    res))
+
+(defun my-stuff-ns-name ()
+  (let ((first-form (my-first-form)))
+    (symbol-name (my-node (second (my-node first-form :children))
+                          :value))))
+
+
+(defun my-stuff-rename-buffer-ns ()
+  (interactive)
+  (rename-buffer (my-ns-name)))
